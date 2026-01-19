3d922f1947c2043ce86ddf75a6e3c7c7e2f5dae9
ZDesign6 <zd7business@gmail.com>
2026-01-19 16:56:54 -0500

    Attmpeted camera juicing, it's bad
    should clean this code up and then do cinemachine instead

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
-    public float cameraXOffset = 6.15f;
+    // -- REFERENCES --
+
     //ref to the Player's Gameobj
     GameObject playerObj;
+
+    // -- CAMERA-BEHAVIOR --
+
+    //How far to the right of the PLayer the camera's destination is. In other words, how far left the Player will be on screen.
+    public float cameraXOffset = 6.15f;
+    //An abstraction of the camera's final destination, equal to the Playet's X plus the offset. Updated every frame.
+    Vector3 destinationPos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,7 +26,12 @@ public class CameraBehavior : MonoBehaviour
     {
         //abstract currentPos for easy referencing
         Vector3 currentPos = this.gameObject.GetComponent<Transform>().position;
-        //set the camera's X equal to the Player's x, plus an offset
-        this.gameObject.GetComponent<Transform>().position = new Vector3(playerObj.GetComponent<Transform>().position.x + cameraXOffset, currentPos.y, currentPos.z);
+        //update the Camera's destination by taking the Player's x and adding the offset
+        destinationPos = new Vector3(playerObj.GetComponent<Transform>().position.x + cameraXOffset, currentPos.y, currentPos.z);
+
+        // -- FINALIZATION/ ASSIGNMENT --
+
+        //move towards destination position
+        this.gameObject.GetComponent<Transform>().position = Vector3.MoveTowards(currentPos, destinationPos, .1f);
     }
 }
